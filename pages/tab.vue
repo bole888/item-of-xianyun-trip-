16556034dd70f5bd1440c6cae3c5249f1bbc1d82
贡献者名字 <aaabbbccc@qq.com>
2020-01-16 21:03:23 +0800

    first commit

---
+++ pages/tab.vue
@@ -0,0 +1,54 @@
+<template>
+   <div class="search-bar">
+        <div class="button">
+            <span v-for="(item,index) in ['新闻','体育','娱乐']" :key="index" 
+            @click="change(index)" :class="{active:current=index}">{{item}}</span>
+        </div>
+
+        <div v-if="current===0">
+            <ul>
+                <li>伊朗</li>
+                <li>伊朗</li>
+                <li>伊朗</li>
+                
+            </ul>
+        </div>
+        <div v-if="current===1">
+            <ul>
+                <li>国足</li>
+                <li>国足</li>
+                <li>国足</li>
+                
+            </ul>
+        </div>
+        <div v-if="current===2">
+            <ul>
+                <li>电影</li>
+                <li>电影</li>
+                <li>电影</li>    
+            </ul>
+        </div>
+   </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            current:0
+        }
+    },
+    methods: {
+        change(index){
+            this.active=index
+        }
+    }
+
+}
+</script>
+
+<style>
+.active{
+    color:red;
+}
+</style>
---
+++ pages/user/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>用户登录页面</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
